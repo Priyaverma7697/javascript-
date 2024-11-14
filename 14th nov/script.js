d0f932504f8bb68192bea49020cfeeb4d3fcb902
Priya Verma <priyaverma1994.et@gmail.com>
2024-11-14 18:48:43 +0530

    styling added 14th november

---
+++ 14th nov/script.js
@@ -0,0 +1,87 @@
+//variable
+// var firstname ="john";
+// {
+//     let lastname="doe";
+//     const age= 35;
+//     console.log (lastname, age)
+
+// }
+
+// let num1= 6565654545n;
+// console.log(typeof num1);
+
+//  console.log("hello");
+//  document.write("hello javascript");
+//  alert("welcome");
+
+// document.getElementById("demo").innerHTML = "Hello world";
+// document.getElementById("demo").innerHTML="welcome to javascript";
+
+// changing the inner html -->
+// var h1=document.getElementById("demo");
+// h1.innerHTML="Welcome to JavaScript";
+
+// // changing the css of element --> 
+// h1.style.color= "red";
+// h1.style.backgroundColor= "aqua";
+
+//padding , border , fontsize, height, apply
+// h1.style.padding="5px";
+// h1.style.border=" 5px solid black";
+// h1.style.fontSize="50px";
+// h1.style.height="80px";
+
+
+// var btn=document.getElementById("btn");
+// btn.style.width="150px";
+// btn.style.height="50px";
+// btn.innerHTML="Show";
+// btn.style.borderRadius="8px";
+// btn.style.fontFamily="Arial";
+// btn.style.fontSize="20px";
+// btn.style.backgroundColor="gray";
+// btn.style.boxShadow="10px 20px 30px gray";
+
+var h1=document.getElementById("demo");
+h1.innerHTML="Welcome to JavaScript"; 
+h1.style.color= "red";
+h1.style.backgroundColor= "#b3b3ff";
+h1.style.padding="15px";
+h1.style.border=" 5px solid black";
+h1.style.fontSize="50px";
+h1.style.height="80px";
+h1.style.textAlign="center";
+h1.style.textShadow="5px 5px 1px gray ";
+h1.style.borderRadius="20px";
+// h1.style.boxShadow="10px 20px 30px gray";
+
+var p=document.getElementById("para");
+p.innerHTML="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis tempore adipisci illum voluptatibus aspernatur rem vero sapiente sunt est odit ducimus, eaque eius ad, vitae veritatis impedit ex dolorum ipsum?";
+p.style.color="black";
+p.style.backgroundColor="#ffb3ff";
+p.style.border="5px solid gray";
+p.style.padding="30px";
+p.style.borderRadius="20px";
+p.style.fontSize="30px";
+p.style.textAlign="center";
+p.style.fontWeight="bold";
+p.style.fontFamily="'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+// p.style.backgroundColor="linear-gradient(to right, red , blue)";
+p.style.textShadow = "5px 5px 1px gray";
+p.style.boxShadow="10px 20px 30px gray";
+
+
+var btn=document.getElementById("btn");
+btn.style.width="150px";
+btn.style.height="50px";
+btn.innerHTML="Show";
+btn.style.borderRadius="8px";
+btn.style.fontFamily="Arial";
+btn.style.fontSize="20px";
+btn.style.backgroundColor="gray";
+btn.style.boxShadow="10px 20px 30px gray";
+btn.style.position="absolute";
+btn.style.left="45%";
+
+
+
