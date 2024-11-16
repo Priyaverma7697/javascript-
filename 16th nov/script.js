5a8af5b82ec76e798ee7cca7ba2e85c2b6faf745
Priya Verma <priyaverma1994.et@gmail.com>
2024-11-16 17:29:12 +0530

    16th nov file added

---
+++ 16th nov/script.js
@@ -0,0 +1,62 @@
+// var x= "hello";
+// var y = "world";
+// var z= x+ " " +y;
+// var a=10;
+// var b=20;
+// let output;
+// output = a+b;
+// output = a-b;
+// output = a*b;
+
+// var a=a+b;
+// a+=b;
+
+//compare only values and === compares value and daya type
+// var num1= 10;
+// var num2= "10";
+// if (num1===num2){
+//     alert("Both are equal");
+// }
+// else{
+//     alert("both are not equal");
+// }
+// ==compare values but does not check data type
+
+//var . let can be reassigned
+//const can be reassigned
+// var global scope
+//
+//var can be redecleared but let and const cannot be redecleared
+
+// {
+// let name="John";
+// name = "Aksahy";
+// console.log(name);
+// }
+
+// var myname="Harray";
+// var myname= "Poter";
+// console.log(myname);
+
+// const number //cannot be reasigned
+// take 3 variables for your name age course and display in alert , console, innerHTML
+
+var h1=document.getElementById("demo");
+
+var myname="Priya Verma";
+var age=25;
+var course="FSD";
+console.log(myname+ " "+age+" "+course);
+
+h1.innerHTML=myname+ " "+age+" "+course;
+
+alert(myname);
+
+//declear 5 variables for name, age, city, course and country
+//declear 2 string variables and concat than into one variable
+//declear 3 variable for firstname, lastname, hobby  and disply
+//it in innerHTML in one line
+//take user name and age from user and display
+
+
+
