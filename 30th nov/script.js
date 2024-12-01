4222656e341bddb261ed63e429b49b38e9c517da
Priya Verma <priyaverma1994.et@gmail.com>
2024-12-01 09:48:53 +0530

    30th nov function templete literal or string litral

---
+++ 30th nov/script.js
@@ -0,0 +1,22 @@
+var para = "hello";
+var name= "john";
+var data=`this is a string`
+let h1 = document.getElementById("demo");
+let div= document.getElementById("trial")
+h1.innerHTML=`hi this is the ans ${para} ${name} ${data}`;
+div.innerHTML= `<h1> ${name} <h1>`;
+//templete literal or string litral
+
+function display(){
+    var name="rose";
+    var city= "bhopal";
+    h1.innerHTML=`${name} ${city}`;
+}
+
+
+
+
+
+
+
+
